23d88a78d16164ea8dba1910bd8b64317140b441
Ritik <ritikraushan160@gmail.com>
2024-05-14 20:54:25 +0530

    commit

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -1,8 +1,13 @@
 const currencies = [
-    "EUR", "USD", "JPY", "BGN", "CZK", "DKK", "GBP", "HUF", "PLN", "RON",
-    "SEK", "CHF", "ISK", "NOK", "HRK", "RUB", "TRY", "AUD", "BRL", "CAD",
-    "CNY", "HKD", "IDR", "ILS", "INR", "KRW", "MXN", "MYR", "NZD", "PHP",
-    "SGD", "THB", "ZAR"
+    "EUR (Euro)", "USD (United States Dollar)", "JPY (Japanese Yen)", "BGN (Bulgarian Lev)",
+    "CZK (Czech Koruna)", "DKK (Danish Krone)", "GBP (British Pound Sterling)", "HUF (Hungarian Forint)",
+    "PLN (Polish Złoty)", "RON (Romanian Leu)", "SEK (Swedish Krona)", "CHF (Swiss Franc)",
+    "ISK (Icelandic Króna)", "NOK (Norwegian Krone)", "HRK (Croatian Kuna)", "RUB (Russian Ruble)",
+    "TRY (Turkish Lira)", "AUD (Australian Dollar)", "BRL (Brazilian Real)", "CAD (Canadian Dollar)",
+    "CNY (Chinese Yuan)", "HKD (Hong Kong Dollar)", "IDR (Indonesian Rupiah)", "ILS (Israeli New Shekel)",
+    "INR (Indian Rupee)", "KRW (South Korean Won)", "MXN (Mexican Peso)", "MYR (Malaysian Ringgit)",
+    "NZD (New Zealand Dollar)", "PHP (Philippine Peso)", "SGD (Singapore Dollar)", "THB (Thai Baht)",
+    "ZAR (South African Rand)"
 ];
 
 export default currencies;
